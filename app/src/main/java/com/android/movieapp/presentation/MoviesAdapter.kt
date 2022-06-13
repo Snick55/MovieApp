1e00e5cdf79902cd223e7c80c774b5fbb03ca699
Snick55 <snikerss123321@gmail.com>
2022-06-13 12:52:03 +0300

    fix

--- app/src/main/java/com/android/movieapp/presentation/MoviesAdapter.kt
+++ app/src/main/java/com/android/movieapp/presentation/MoviesAdapter.kt
@@ -4,7 +4,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 import com.android.movieapp.data.cloud.Film
+import com.android.movieapp.databinding.CategoryItemBinding
 import com.android.movieapp.databinding.MovieItemBinding
 import com.bumptech.glide.Glide
 
@@ -19,24 +21,38 @@ class MoviesAdapter : RecyclerView.Adapter<MoviesAdapter.MyViewHolder>() {
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        val binding = MovieItemBinding.inflate(inflater, parent, false)
-        return MyViewHolder(binding)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val poster = films[position].imageUrl
-        val name = films[position].localizedName
-        with(holder.binding) {
-            root.tag = films[position]
-            nameItem.text = name
-            Glide.with(root.context).load(poster).into(background)
+        return if (viewType == 0) {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = CategoryItemBinding.inflate(inflater, parent, false)
+            MyViewHolder(binding)
+        } else {
+            val inflater = LayoutInflater.from(parent.context)
+            val binding = MovieItemBinding.inflate(inflater, parent, false)
+            MyViewHolder(binding)
         }
     }
 
-    override fun getItemCount(): Int = films.size
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        if (holder.binding is MovieItemBinding) {
+            val poster = films[position].imageUrl
+            val name = films[position].localizedName
+            with(holder.binding) {
+                root.tag = films[position]
+                nameItem.text = name
+                Glide.with(root.context).load(poster).into(background)
+            }
+        }
+    }
 
-    class MyViewHolder(val binding: MovieItemBinding) : RecyclerView.ViewHolder(binding.root)
+    override fun getItemCount(): Int = films.size 
+
+    override fun getItemViewType(position: Int): Int {
+        // TODO: 13.06.2022  
+        return if (position == 0 || position == 1 || position == 2) 0
+        else 1
+    }
+
+    class MyViewHolder(val binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)
 
 
 }
